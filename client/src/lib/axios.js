a7948473230b75fd70787ed6acbd1830a23bdeb9
sailajasuprava <sailaja.suprava@gmail.com>
2025-04-29 11:06:50 +0530

    backend url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://yardstick-task-nine.vercel.app/api",
   withCredentials: true, // send cookies to the server
 });
 
